ab656d78d8fd09749aa6973dd646246c64ad6623
Kirill Shatskiy <shaackiy@gmail.com>
2017-05-24 11:10:12 +0300

    Gradient on text

--- dist/bundle.js
+++ dist/bundle.js
@@ -83,7 +83,7 @@ function canvasApp() {
     }
 
     var appElement = document.getElementById('app');
-    var appTemplate = '<canvas id="canvasOne" width="500" height="500">\n        Your browser doesn\'t support HTML5 canvas.\n        </canvas>\n        <form>\n        Text: <input type="text" id="textBox" placeholder="some text"/><br>\n        Fill or stroke:\n        <select id="fillOrStroke">\n            <option value="fill">fill</option>\n            <option value="stroke">stroke</option>\n            <option value="both">both</option>\n        </select><br>\n        Font style: <select id="fontStyle">\n            <option value="normal">normal</option>\n            <option value="italic">italic</option>\n            <option value="oblique">oblique</option>\n        </select><br>\n        Font weight: <select id="fontWeight">\n            <option value="normal">normal</option>\n            <option value="bold">bold</option>\n            <option value="bolder">bolder</option>\n            <option value="lighter">lighter</option>\n        </select><br>\n        Font: <select id="fontFace">\n            <option value="serif">serif</option>\n            <option value="sans-serif">sans-serif</option>\n            <option value="cursive">cursive</option>\n            <option value="fantasy">fantasy</option>\n            <option value="monospace">monospace</option>\n        </select><br>\n        Font size: <input type="range" id="textSize" min="0" max="200" step="1" value="50"/><br>\n        Color: <input type="color" id="textColor"/><br>\n        </form>';
+    var appTemplate = '<canvas id="canvasOne" width="500" height="500">\n        Your browser doesn\'t support HTML5 canvas.\n        </canvas>\n        <form>\n        Text: <input type="text" id="textBox" placeholder="some text"/><br>\n        Fill or stroke:\n        <select id="fillOrStroke">\n            <option value="fill">fill</option>\n            <option value="stroke">stroke</option>\n            <option value="both">both</option>\n        </select><br>\n        <select id="fillType">\n            <option value="none">None</option>\n            <option value="linear">Linear Gradient</option>\n            <option value="radial">Radial Gradient</option>\n        </select><br>\n        Font style: <select id="fontStyle">\n            <option value="normal">normal</option>\n            <option value="italic">italic</option>\n            <option value="oblique">oblique</option>\n        </select><br>\n        Font weight: <select id="fontWeight">\n            <option value="normal">normal</option>\n            <option value="bold">bold</option>\n            <option value="bolder">bolder</option>\n            <option value="lighter">lighter</option>\n        </select><br>\n        Font: <select id="fontFace">\n            <option value="serif">serif</option>\n            <option value="sans-serif">sans-serif</option>\n            <option value="cursive">cursive</option>\n            <option value="fantasy">fantasy</option>\n            <option value="monospace">monospace</option>\n        </select><br>\n        Font size: <input type="range" id="textSize" min="0" max="200" step="1" value="50"/><br>\n        Color 1: <input type="color" id="textColor1"/><br>\n        Color 2: <input type="color" id="textColor2"/><br>\n        TextAlpha: <input type="range" id="textAlpha" min="0.0" max="1.0" step="0.01" value="1.0"/><br>\n        shadowX: <input type="range" id="shadowX" min="-100" max="100" step="1" value="1"/><br>\n        shadowY: <input type="range" id="shadowY" min="-100" max="100" step="1" value="1"/><br>\n        shadowBlur: <input type="range" id="shadowBlur" min="0.0" max="1.0" step="0.01" value="1"/><br>\n        Color: <input type="color" id="shadowColor" value="#707070"/><br>\n        </form>';
 
     appElement.innerHTML = appTemplate;
 
@@ -93,11 +93,18 @@ function canvasApp() {
     var appState = {
         message: 'some text',
         fillOrStroke: 'fill',
-        textSize: '50',
+        textSize: 50,
         fontFace: 'serif',
         fontWeight: 'normal',
         fontStyle: 'normal',
-        textColor: '#ff0000'
+        textColor1: '#000000',
+        textColor2: '#ff0000',
+        textAlpha: '1.0',
+        shadowX: 1,
+        shadowY: 1,
+        shadowBlur: 1,
+        shadowColor: '#707070',
+        fillType: 'none'
     };
 
     function clearRect() {
@@ -113,6 +120,9 @@ function canvasApp() {
 
     function drawScreen() {
         clearRect();
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+        context.globalAlpha = 1;
 
         context.fillStyle = 'gray';
         context.fillRect(0, 0, 500, 500);
@@ -120,24 +130,52 @@ function canvasApp() {
         context.strokeStyle = 'black';
         context.strokeRect(10, 10, 480, 480);
 
+        context.globalAlpha = appState.textAlpha;
+
+        context.shadowOffsetX = appState.shadowX;
+        context.shadowOffsetY = appState.shadowY;
+        context.shadowBlur = appState.shadowBlur;
+        context.shadowColor = appState.shadowColor;
+
         context.font = getFontString(appState);
-        console.log(getFontString(appState));
         var metrics = context.measureText(appState.message);
         var textWidth = metrics.width;
+
+        var textColor = void 0;
+
+        switch (appState.fillType) {
+            case 'none':
+                textColor = appState.textColor1;
+                break;
+            case 'linear':
+                var linearGradient = context.createLinearGradient(100, 100, textWidth, 100);
+                linearGradient.addColorStop(0, appState.textColor1);
+                linearGradient.addColorStop(0.5, appState.textColor2);
+                textColor = linearGradient;
+                break;
+            case 'radial':
+                var radialGradient = context.createRadialGradient(250, 250, 300, 250, 250, 5);
+                console.log(appState.textColor1, appState.textColor2);
+                radialGradient.addColorStop(0, appState.textColor1);
+                radialGradient.addColorStop(1, appState.textColor2);
+                textColor = radialGradient;
+                break;
+        }
+
         var xPosition = theCanvas.width / 2 - textWidth / 2;
         var yPosition = theCanvas.height / 2;
 
         switch (appState.fillOrStroke) {
             case 'fill':
-                context.fillStyle = appState.textColor;
+                context.fillStyle = textColor;
                 context.fillText(appState.message, xPosition, yPosition);
                 break;
             case 'stroke':
-                context.strokeStyle = appState.textColor;
+                context.strokeStyle = textColor;
                 context.strokeText(appState.message, xPosition, yPosition);
                 break;
             case 'both':
-                context.fillStyle = appState.textColor;
+                context.fillStyle = textColor;
                 context.fillText(appState.message, xPosition, yPosition);
                 context.strokeStyle = '#000000';
                 context.strokeText(appState.message, xPosition, yPosition);
@@ -163,7 +201,16 @@ function canvasApp() {
     changeAppStateOnDOMEvent('fontWeight', 'fontWeight', 'change');
     changeAppStateOnDOMEvent('fontFace', 'fontFace', 'change');
     changeAppStateOnDOMEvent('textSize', 'textSize', 'change');
-    changeAppStateOnDOMEvent('textColor', 'textColor', 'change');
+    changeAppStateOnDOMEvent('textColor1', 'textColor1', 'change');
+    changeAppStateOnDOMEvent('textColor2', 'textColor2', 'change');
+    changeAppStateOnDOMEvent('textAlpha', 'textAlpha', 'change');
+
+    changeAppStateOnDOMEvent('shadowX', 'shadowX', 'change');
+    changeAppStateOnDOMEvent('shadowY', 'shadowY', 'change');
+    changeAppStateOnDOMEvent('shadowBlur', 'shadowBlur', 'change');
+    changeAppStateOnDOMEvent('shadowColor', 'shadowColor', 'change');
+
+    changeAppStateOnDOMEvent('fillType', 'fillType', 'change');
 
     drawScreen();
 }
--- src/ch3-text-api/index.js
+++ src/ch3-text-api/index.js
@@ -16,6 +16,11 @@ export function canvasApp() {
             <option value="stroke">stroke</option>
             <option value="both">both</option>
         </select><br>
+        <select id="fillType">
+            <option value="none">None</option>
+            <option value="linear">Linear Gradient</option>
+            <option value="radial">Radial Gradient</option>
+        </select><br>
         Font style: <select id="fontStyle">
             <option value="normal">normal</option>
             <option value="italic">italic</option>
@@ -35,7 +40,13 @@ export function canvasApp() {
             <option value="monospace">monospace</option>
         </select><br>
         Font size: <input type="range" id="textSize" min="0" max="200" step="1" value="50"/><br>
-        Color: <input type="color" id="textColor"/><br>
+        Color 1: <input type="color" id="textColor1"/><br>
+        Color 2: <input type="color" id="textColor2"/><br>
+        TextAlpha: <input type="range" id="textAlpha" min="0.0" max="1.0" step="0.01" value="1.0"/><br>
+        shadowX: <input type="range" id="shadowX" min="-100" max="100" step="1" value="1"/><br>
+        shadowY: <input type="range" id="shadowY" min="-100" max="100" step="1" value="1"/><br>
+        shadowBlur: <input type="range" id="shadowBlur" min="0.0" max="1.0" step="0.01" value="1"/><br>
+        Color: <input type="color" id="shadowColor" value="#707070"/><br>
         </form>`;
 
     appElement.innerHTML = appTemplate;
@@ -46,11 +57,18 @@ export function canvasApp() {
     const appState = {
         message: 'some text',
         fillOrStroke: 'fill',
-        textSize: '50',
+        textSize: 50,
         fontFace: 'serif',
         fontWeight: 'normal',
         fontStyle: 'normal',
-        textColor: '#ff0000',
+        textColor1: '#000000',
+        textColor2: '#ff0000',
+        textAlpha: '1.0',
+        shadowX: 1,
+        shadowY: 1,
+        shadowBlur: 1,
+        shadowColor: '#707070',
+        fillType: 'none'
     };
 
     function clearRect() {
@@ -64,30 +82,62 @@ export function canvasApp() {
 
     function drawScreen() {
         clearRect();
-        
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+        context.globalAlpha = 1;
+
         context.fillStyle = 'gray';
         context.fillRect(0, 0, 500, 500);
 
         context.strokeStyle = 'black';
         context.strokeRect(10, 10, 480, 480);
 
+        context.globalAlpha = appState.textAlpha;
+
+        context.shadowOffsetX = appState.shadowX;
+        context.shadowOffsetY = appState.shadowY;
+        context.shadowBlur = appState.shadowBlur;
+        context.shadowColor = appState.shadowColor;
+
         context.font = getFontString(appState);
         const metrics = context.measureText(appState.message);
         const textWidth = metrics.width;
+
+        let textColor;
+
+        switch(appState.fillType) {
+            case 'none':
+                textColor = appState.textColor1;
+                break;
+            case 'linear':
+                const linearGradient = context.createLinearGradient(100, 100, textWidth, 100);
+                linearGradient.addColorStop(0, appState.textColor1);
+                linearGradient.addColorStop(0.5, appState.textColor2);
+                textColor = linearGradient;
+                break;
+            case 'radial':
+                const radialGradient = context.createRadialGradient(250, 250, 300, 250, 250, 5);
+                console.log(appState.textColor1, appState.textColor2);
+                radialGradient.addColorStop(0, appState.textColor1);
+                radialGradient.addColorStop(1, appState.textColor2);
+                textColor = radialGradient;
+                break;
+        }
+
         const xPosition = (theCanvas.width / 2) - (textWidth / 2);
         const yPosition = theCanvas.height / 2;
 
         switch(appState.fillOrStroke) {
             case 'fill':
-                context.fillStyle = appState.textColor;
+                context.fillStyle = textColor;
                 context.fillText(appState.message, xPosition, yPosition);
                 break;
             case 'stroke':
-                context.strokeStyle = appState.textColor;
+                context.strokeStyle = textColor;
                 context.strokeText(appState.message, xPosition, yPosition);
                 break;
             case 'both':
-                context.fillStyle = appState.textColor;
+                context.fillStyle = textColor;
                 context.fillText(appState.message, xPosition, yPosition);
                 context.strokeStyle = '#000000';
                 context.strokeText(appState.message, xPosition, yPosition);
@@ -112,7 +162,16 @@ export function canvasApp() {
     changeAppStateOnDOMEvent('fontWeight', 'fontWeight', 'change');
     changeAppStateOnDOMEvent('fontFace', 'fontFace', 'change');
     changeAppStateOnDOMEvent('textSize', 'textSize', 'change');
-    changeAppStateOnDOMEvent('textColor', 'textColor', 'change');
+    changeAppStateOnDOMEvent('textColor1', 'textColor1', 'change');
+    changeAppStateOnDOMEvent('textColor2', 'textColor2', 'change');
+    changeAppStateOnDOMEvent('textAlpha', 'textAlpha', 'change');
+
+    changeAppStateOnDOMEvent('shadowX', 'shadowX', 'change');
+    changeAppStateOnDOMEvent('shadowY', 'shadowY', 'change');
+    changeAppStateOnDOMEvent('shadowBlur', 'shadowBlur', 'change');
+    changeAppStateOnDOMEvent('shadowColor', 'shadowColor', 'change');
+
+    changeAppStateOnDOMEvent('fillType', 'fillType', 'change');
 
     drawScreen();
 }
