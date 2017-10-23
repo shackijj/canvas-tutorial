1ef9ed0b309e275f3d62cd0db994ccae854d91c6
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2017-10-23 12:05:45 +0300

    Start puzzle.

--- src/ch6-audio-and-video/index.js
+++ src/ch6-audio-and-video/index.js
@@ -5,32 +5,84 @@ export function canvasApp() {
 
     const appElement = document.getElementById('app');
     const appTemplate = 
-        `<video autoplay loop controls id="theVideo" width="320" height="240">
-            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-            <source src="https://www.w3schools.com/html/mov_bbb.ogg" type="video/ogg">
+        `<video autoplay loop controls id="theVideo" width="320" height="240" style="display: none;">
+            <source src="http://design.enter-media.org/upload/iblock/2ae/2aed972c5726025ebd0eedc378866a61.webm" type="video/webm">
         </video>
-        <form>
-            Video Size: <input type="range" id="videoSize"
-            min="80"
-            max="1280"
-            step="1"
-            value="320"/>
-        </form>`;
+        <canvas id="theCanvas" width="370" height="290">
+
+        </canvas>`;
 
     appElement.innerHTML = appTemplate;
 
-    let aspectRatio;
+    const videoElement = document.getElementById('theVideo');
+    const canvasElement = document.getElementById('theCanvas');
+    const context = canvasElement.getContext('2d');
+    const videoWidth = 1920;
+    const videoHeight = 1080;
 
-    function onSizeChange(event) {
-        const { target } = event;
-        const video = document.getElementById('theVideo');
-        video.width = target.value;
-        video.height = target.value / aspectRatio;
+    const rows = 4;
+    const cols = 4;
+    const xPad = 10;
+    const yPad = 10;
+    const startXOffset = 10;
+    const startYOffset = 10;
+    const partWidth = videoElement.width / rows;
+    const partHeight = videoElement.height / cols;
+
+    function randomizeBoard(board) {
+        return board;
     }
 
-    const video = document.getElementById('theVideo');
-    aspectRatio = video.width / video.height;
+    function initBoard(rows, cols) {
+        const board = [];
+        for(let i = 0; i < rows; i++) {
+            board[i] = [];
+            for(let j = 0; j < cols; j++) {
+                board[i][j] = { finalRow: i, finalCol: j, selected: false };
+            }
+        }
+        return board;
+    }
 
-    const input = document.getElementById('videoSize');
-    input.addEventListener('change', onSizeChange);
+    const board = randomizeBoard(initBoard(rows, cols));
+
+    function drawScreen() {
+        context.fillStyle = '#FFFFFF';
+        context.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+        context.strokeStyle = '#303030';
+        context.strokeRect(5, 5, theCanvas.width - 10, theCanvas.height - 10);
+
+        for(let r = 0; r < rows; r++) {
+            for(let c = 0; c < cols; c++) {
+                const tempPiece = board[r][c];
+                const { finalRow, finalCol, selected } = tempPiece;
+
+                const partVideoWidth = videoWidth / cols;
+                const partVideoHeight = videoHeight / rows;
+
+                const imageX = finalCol * partVideoWidth;
+                const imageY = finalRow * partVideoHeight;
+
+
+                const placeX = startXOffset + (xPad * c) + (partWidth * c);
+                const placeY = startYOffset + (yPad * r) + (partHeight * r);
+                
+                context.drawImage(videoElement, imageX, imageY, partVideoWidth, partVideoHeight,
+                    placeX, placeY, partWidth, partHeight);
+                
+                if (tempPiece.selected) {
+                    context.strokeStyle = '#FFFF00';
+                    context.strokeRect(placeX, placeY, partWidth, partHeight);
+                }
+            }
+        }
+    }
+
+    function gameLoop() {
+        drawScreen();
+        window.requestAnimationFrame(gameLoop);
+    }
+
+    window.requestAnimationFrame(gameLoop);
 }
