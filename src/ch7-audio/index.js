031b6e78ea64e86f44914c27643276493ed833f3
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2017-11-13 11:47:34 +0300

    Custom audio controls on canvas

--- src/ch7-audio/index.js
+++ src/ch7-audio/index.js
@@ -27,20 +27,76 @@ export function canvasApp() {
     const sliderW = 10;
     const sliderH = 32;
     const controlStartX = 25;
-    const controlStartY = 200;
+    const controlStartY = 300;
 
     const playX = controlStartX;
     const playY = controlStartY;
     const playBackX = controlStartX + bW;
-    const playBackY = controlStartY + bW;
+    const playBackY = controlStartY;
     const volBackX = controlStartX + bW + playBackW;
     const volBackY = controlStartY;
     const loopX = controlStartX + bW + playBackW + volBackW;
     const loopY = controlStartY;
 
+    const volumeSliderStart = volBackX;
+    const volumeSliderEnd = volBackX + volBackW - sliderW;
+
+    const buttonWait = 30;
+    let timeWaited = 0;
+    let mouseX;
+    let mouseY;
+    let volumeSliderDrag;
+    let volumeSliderX;
+    let volumeSliderY;
+
     function drawScreen() {
         context.fillStyle = '#aaaaaa';
         context.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+        context.drawImage(buttonsSheet, 32, 0, playBackW, bH, playBackX, playBackY, playBackW, bH);
+
+        const slideIncrement = playBackW / theAudio.duration;
+        const sliderX = (controlStartX + bW) + (slideIncrement * theAudio.currentTime);
+        context.drawImage(buttonsSheet, 238, 0, sliderW, bH, sliderX, controlStartY, sliderW, bH);
+
+        if (theAudio.ended&& !theAudio.loop) {
+            theAudio.currentTime = 0;
+            theAudio.pause();
+        }
+
+        if (theAudio.paused) {
+            context.drawImage(buttonsSheet, 0, 0, bW, bH, playX, playY, bW, bH)
+        } else {
+            context.drawImage(buttonsSheet, 0, 32, bW, bH, playX, playY, bW, bH)
+        }
+
+        if (theAudio.loop) {
+            context.drawImage(buttonsSheet, 113, 32, bW, bH, loopX, loopY, bW, bH)
+        } else {
+            context.drawImage(buttonsSheet, 81, 32, bW, bH, loopX, loopY, bW, bH)
+        }
+
+        volumeSliderX = volumeSliderStart + (theAudio.volume * (volBackW - sliderW));
+        volumeSliderY = controlStartY;
+        const volumeIncrement = 1 / (volBackW - sliderW);
+
+        context.drawImage(buttonsSheet, 32, 32, volBackW, bH, volBackX, volBackY, volBackW, bH);
+        if (volumeSliderDrag) {
+            volumeSliderX = mouseX;
+            if (volumeSliderX > volumeSliderEnd) {
+                volumeSliderX = volumeSliderEnd;
+            }
+            if (volumeSliderX < volumeSliderStart) {
+                volumeSliderX = volumeSliderStart;
+            }
+        } else {
+            volumeSliderX = volumeSliderStart + (theAudio.volume * (volBackW - sliderW));
+        }
+
+        context.drawImage(buttonsSheet, 238, 0, sliderW, bH, volumeSliderX, volumeSliderY, sliderW, bH);
+        theAudio.volume = (volumeSliderX - volumeSliderStart) * volumeIncrement;
+
+        timeWaited++;
 
         context.fillStyle = '#000000';
         context.fillText(`duration: ${theAudio.duration}`, 20, 20);
@@ -73,6 +129,57 @@ export function canvasApp() {
             gameLoop();
         }
     }
+
+    function eventMouseUp(event) {
+        if (timeWaited >= buttonWait) {
+            timeWaited = 0;
+            if ((mouseY >= playY) && (mouseY <= playY + bH) && (mouseX >= playX) && (mouseX <= playX + bW)) {
+                if (theAudio.paused) {
+                    theAudio.play();
+                } else {
+                    theAudio.pause();
+                }
+            }
+
+            if ((mouseY >= loopY) && (mouseY <= loopY + bH) && (mouseX >= loopX) && (mouseX <= loopX + bW)) {
+                if (theAudio.loop) {
+                    theAudio.loop = false;
+                } else {
+                    theAudio.loop = true;
+                }
+            }
+        }
+
+        if (volumeSliderDrag) {
+            volumeSliderDrag = false;
+        }
+    }
+    function eventMouseDown(event) {
+        if ((mouseX >= volumeSliderX) && (mouseX <= volumeSliderX + sliderW) && (mouseY >= volumeSliderY) && (mouseY <= volumeSliderY + sliderH)) {
+            volumeSliderDrag = true;
+        }
+    }
+    function eventMouseMove(event) {
+        let x;
+        let y;
+
+        if (event.pageX || event.pageY) {
+            x = event.pageX;
+            y = event.pageY;
+        } else {
+            x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+
+        x = x - theCanvas.offsetLeft;
+        y = y - theCanvas.offsetTop;
+        mouseX = x;
+        mouseY = y;
+    }
+
+    theCanvas.addEventListener('mouseup', eventMouseUp, false);
+    theCanvas.addEventListener('mousedown', eventMouseDown, false);
+    theCanvas.addEventListener('mousemove', eventMouseMove, false);
 
     buttonsSheet.onload = itemLoaded;
     buttonsSheet.src = 'audiocontrols.png';
